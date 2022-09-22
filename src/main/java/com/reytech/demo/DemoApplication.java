4e35ec83606658b5cd77216e8aa0fdfb4af2ff14
SilvereCpp <78033128+SilvereCpp@users.noreply.github.com>
2022-09-22 12:02:31 +0200

    Update DemoApplication.java

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.ruen(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
